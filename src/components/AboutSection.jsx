bee22521ed6a7b39dc260c5c897a624b7e6394d6
Akshat Agrawal <agrawalakshat108@gmail.com>
2025-12-08 16:54:45 +0530

    Updated portfolio changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/AKSHAT_AGRAWAL,IIITM_.pdf"
+                href="/Akshat_Agrawal_CV.pdf"
                 download="Akshat_Agrawal_CV"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
